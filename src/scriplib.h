06cc722f642425d77d2838922dccc007f253cc48
Gaspar <gasparguidopavon@gmail.com>
2025-01-09 00:11:40 -0300

    Clean scriplib

--- src/scriplib.h
+++ src/scriplib.h
@@ -3,8 +3,7 @@
 #define MAXTOKEN 512
 
 extern char token[MAXTOKEN];
-extern char *scriptbuffer, *script_p, *scriptend_p;
-extern int grabbed;
+extern char *script_p;
 extern int scriptline;
 extern qboolean endofscript;
 
